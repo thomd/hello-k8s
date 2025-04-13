f38b66d1d9378486ea4482be5d1257aae8c8a9fe
Thomas Dürr <thomduerr@gmail.com>
2025-04-13 18:37:03 +0200

    v0.0.2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Kubernetes via Argo CD"
+    return "Argo CD Rules"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
